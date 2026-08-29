44ec9314b05ce983ff7303c7c20af7493e5fe79a
mierzcha <mierzcha@th-brandenburg.de>
2026-08-29 12:40:01 +0200

    Update client.py

--- src/llm/client.py
+++ src/llm/client.py
@@ -7,22 +7,6 @@ load_dotenv()
 class OllamaClient:
     """
     A class used to represent an Ollama Client
-
-    Attributes
-
-    says_str : str
-        a formatted string to print out what the animal says
-    name : str
-        the name of the animal
-    sound : str
-        the sound that the animal makes
-    num_legs : int
-        the number of legs the animal has (default 4)
-
-    Methods
-    -------
-    says(sound=None)
-        Prints the animals name and what sound it makes
     """
     def __init__(self, url: str, model: str):
         """
