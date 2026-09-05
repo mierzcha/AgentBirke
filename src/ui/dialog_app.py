dfcdd370da2b6034a9b3697fe2c244fbf48d8669
mierzcha <mierzcha@dipa.th-brandenburg.de>
2026-09-05 13:29:13 +0200

    WIP Dialogsystem - Anzeige des aktuellen Dialogzustands

--- src/ui/dialog_app.py
+++ src/ui/dialog_app.py
@@ -10,63 +10,125 @@ st.set_page_config(
 )
 
 st.title("🌳 Agent Birke")
-st.header("Dialogsteuerung")
+st.header("Dialogsystem")
+
+
+# Zustandsautomat initialisieren
 
 if "state_machine" not in st.session_state:
     st.session_state.state_machine = DialogStateMachine()
 
 state_machine = st.session_state.state_machine
 
+# Berührung
+
+if "touch_active" not in st.session_state:
+    st.session_state.touch_active = False
+
+
+# Aktueller Dialogzustand
 
 st.subheader("Aktueller Dialogzustand")
 
 st.info(state_machine.state.value)
 
-#todo Umweltbedingungen, log
+# Umweltbedingungen
 
-touchBtnTxt="Birke berühren"
+st.subheader("Umweltbedingungen")
 
+st.write("TODO: Aktive Umweltbedingungen anzeigen")
+
+# Aktionen
 
 st.subheader("Aktionen")
 
 col1, col2, col3 = st.columns(3)
 
+
 with col1:
-    if st.button(touchBtnTxt): #todo: Button soll verschwinden wenn berührung aktiv, stattdessen soll der button Birke loslassen werden
-        state_machine.handle_event("touch")
-        touchBtnTxt="Birke loslassen"
-        st.rerun()
+
+    if not st.session_state.touch_active:
+
+        if st.button("Birke berühren"):
+            st.session_state.touch_active = True
+
+            state_machine.handle_event("touch")
+
+            st.rerun()
+
+    else:
+
+        if st.button("Birke loslassen"):
+            st.session_state.touch_active = False
+
+            st.rerun()
+
 
 with col2:
+
     if st.button("Spracheingabe"):
+
+        # Spracheingabe wird später durch STT ersetzt
         state_machine.handle_event("speech")
-        #todo 
+
         st.rerun()
 
+
 with col3:
-    if st.button("WIP Giessen"):
-        #todo
-        print(f"soil_moisture erhöhen")
+
+    if st.button("WIP Gießen"):
+
+        # TODO Wird später mit dem EnvironmentSimulator verbunden
+        st.write("Bodenfeuchtigkeit erhöhen")
 
 
-st.subheader("Dialogzustand manuell setzen")
+# Dialogverlauf
+
+st.subheader("Dialogverlauf")
+
+st.write("TODO: Dialogverlauf anzeigen")
+
+# Zustandsautomat
+
+st.subheader("Zustandsautomat")
+
+st.write(
+    "Der aktuelle Zustand des Dialogautomaten ist:"
+)
+
+st.info(state_machine.state.value)
+
+# Entwicklerbereich
+
+st.subheader("Entwicklerbereich")
+
+st.write(
+    "Hier kann der Dialogzustand für Testzwecke "
+    "manuell verändert werden."
+)
 
 selected_state = st.selectbox(
-    "Zustand auswählen",
+    "Dialogzustand auswählen",
     list(DialogState),
     format_func=lambda state: state.value,
 )
 
 if st.button("Zustand übernehmen"):
+
     state_machine.state = selected_state
+
     st.rerun()
 
+# Bearbeitungszeit
 
-st.subheader("Zustandsautomat")
+st.subheader("Bearbeitungszeit")
 
-st.write(
-    "Der aktuelle Zustand wird durch die DialogStateMachine "
-    "verwaltet."
-)
+st.write("TODO: Antwort- bzw. Bearbeitungszeit anzeigen")
 
-st.write(f"**Aktueller Zustand:** {state_machine.state.value}")
+# Spracheingabe und -ausgabe
+
+st.subheader("Sprache")
+
+st.write("🎤 Spracheingabe: TODO")
+
+st.write("🔊 Sprachausgabe: TODO")
