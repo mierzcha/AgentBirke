d112a9cc8af454c618662fa669eab5f7ee727d19
mierzcha <mierzcha@dipa.th-brandenburg.de>
2026-09-05 13:38:13 +0200

    Zustandsverlauf in state_machine.py

--- src/dialog/state_machine.py
+++ src/dialog/state_machine.py
@@ -6,9 +6,12 @@ class DialogStateMachine:
 
     def __init__(self):
         self.state = DialogState.IDLE
+        self.history = [DialogState.IDLE]
 
     def handle_event(self, event: str) -> DialogState:
         """Process an event and return the new dialogue state."""
+
+        old_state = self.state
 
         if self.state == DialogState.IDLE:
             if event == "touch":
@@ -24,5 +27,9 @@ class DialogStateMachine:
 
         elif self.state == DialogState.GOODBYE:
             self.state = DialogState.IDLE
+
+        # Only record actual state changes
+        if self.state != old_state:
+            self.history.append(self.state)
 
         return self.state
